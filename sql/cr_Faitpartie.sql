1c78069f73adbb6696d9fce13a6a3609526752c8
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-02-24 12:18:15 +0100

    Stats alpha + SQL

---
+++ sql/cr_Faitpartie.sql
@@ -0,0 +1,8 @@
+CREATE Faitpartie
+(
+    fap_eleve       INTEGER,
+    fap_promo       INTEGER,
+    PRIMARY KEY(fap_eleve, fap_promo),
+    FOREIGN KEY(fap_eleve) REFERENCES Utilisateur(uti_id),
+    FOREIGN KEY(fap_promo) REFERENCES Promotion(pro_id)
+);
