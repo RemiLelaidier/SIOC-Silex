08c0cff7d8a6b2bee2972ad241f8f9075f4ee472
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-02-25 11:24:42 +0100

    fix 6

--- modeles/DAO/ActiviteDAO.php
+++ modeles/DAO/ActiviteDAO.php
@@ -32,7 +32,7 @@ class ActiviteDAO extends DAO
     // Methode findAll //
     public function findAll()
     {
-        $sql = "SELECT * FROM Activite ORDER BY act_id";
+        $sql = "SELECT * FROM Activite";
         $result = $this->getDb()->fetchAll($sql, array());
 
         // Convertit le resultat de la requete en tableau //
