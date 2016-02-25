af2d74269350feb72dc1e1c228555352ba134b12
SirCrowley <shiky0@live.fr>
2016-02-25 10:41:11 +0100

    Hierarchies des roles etablis

--- controleurs/app.php
+++ controleurs/app.php
@@ -33,28 +33,28 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     ),
 ));
 
+$app['security.role_hierarchy'] = array(
+    'ROLE_ADMIN'    => array('ROLE_PROF', 'ROLE_ELEVE', 'ROLE_USER'),
+    'ROLE_PROF'    => array('ROLE_ELEVE', 'ROLE_USER'),
+    'ROLE_ELEVE'    => array('ROLE_USER')
+);
+
+// Definition des rôles utilisateurs
 //'security.access_rules' => array(
 //    array('^/acceuil', 'ROLE_USER'),
 //    array('^/layout', 'ROLE_USER'),
-//    array('^/layout', 'ROLE_ELEVE'),
 //    array('^/activite/', 'ROLE_ELEVE'),
 //    array('^/activite/new', 'ROLE_ELEVE'),
 //    array('^/competence/', 'ROLE_ELEVE'),
 //    array('^/competence/new', 'ROLE_ELEVE'),
-//    array('^/layout', 'ROLE_PROF'),
 //    array('^/competence/.*$', 'ROLE_PROF'),
 //    array('^/activite/.*$', 'ROLE_PROF'),
 //    array('^/eleves/.*$', 'ROLE_PROF'),
 //    array('^/promotion/.*$', 'ROLE_PROF'),
-//    array('^/competence/.*$', 'ROLE_ADMIN'),
-//    array('^/activite/.*$', 'ROLE_ADMIN'),
-//    array('^/layout', 'ROLE_ADMIN'),
-//    array('^/eleves/.*$', 'ROLE_ADMIN'),
-//    array('^/promotion/.*$', 'ROLE_ADMIN'),
 //    array('^/professeurs/.*$', 'ROLE_ADMIN'),
 //    array('^/', ''), // Cette url est accessible en mode non connecté
 //    array('^/login', ''),
-//)
+//);
 
 // Register services.
 $app['dao.user'] = $app->share(function ($app) {
@@ -75,8 +75,4 @@ $app['dao.promotion'] = $app->share(function ($app) {
 
 $app['dao.utilisateur'] = $app->share(function ($app) {
     return new SIOC\DAO\UtilisateurDAO($app['db']);
-});
-
-$app['dao.associe'] = $app->share(function ($app) {
-    return new SIOC\DAO\AssocieDAO($app['db']);
 });
