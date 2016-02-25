29281863090a2dfd27244c0a53f1a58af40ec3ae
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-02-25 11:25:46 +0100

    fix 7

--- modeles/DAO/CompetenceDAO.php
+++ modeles/DAO/CompetenceDAO.php
@@ -44,8 +44,7 @@ class CompetenceDAO extends DAO
         $sql = "SELECT C.com_id, C.com_reference, C.com_libelle, C.com_description, C.com_obligatoire "
                 . "FROM Competence AS C, Associe AS A "
                 . "WHERE A.ass_competence = C.com_id "
-                . "AND A.ass_activite = ? "
-                . "ORDER BY com_id";
+                . "AND A.ass_activite = ?";
         $result = $this->getDb()->fetchAll($sql, array($activiteId));
                 
         $competences = array();
