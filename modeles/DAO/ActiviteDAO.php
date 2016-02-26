494002cee776635bc5f7122a08dd04cee7d1c92f
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-02-26 11:44:37 +0100

    Fix 3

--- modeles/DAO/ActiviteDAO.php
+++ modeles/DAO/ActiviteDAO.php
@@ -42,7 +42,7 @@ class ActiviteDAO extends DAO
     public function findAll()
     {
         $sql = "SELECT * FROM Activite";
-        $result = $this->getDb()->fetchAll($sql, array());
+        $result = $this->getDb()->fetchAssoc($sql, array());
 
         // Convertit le resultat de la requete en tableau //
         $activites = array();
--- modeles/DAO/CompetenceDAO.php
+++ modeles/DAO/CompetenceDAO.php
@@ -35,7 +35,7 @@ class CompetenceDAO extends DAO
     public function findAll()
     {
         $sql = "SELECT * FROM Competence ORDER BY com_id";
-        $result = $this->getDb()->fetchAll($sql, array());
+        $result = $this->getDb()->fetchAssoc($sql, array());
 
         // Convertit le resultat de la requete en tableau //
         $competences = array();
@@ -58,7 +58,7 @@ class CompetenceDAO extends DAO
                 . " FROM Competence AS C, Associe AS A "
                 . " WHERE A.ass_competence = C.com_id "
                 . " AND A.ass_activite = ?";
-        $result = $this->getDb()->fetchAll($sql, array($activiteId));
+        $result = $this->getDb()->fetchAssoc($sql, array($activiteId));
                 
         $competences = array();
         foreach ($result as $row) {
