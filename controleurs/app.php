5e8d7a4f95217ba23f7fe75adc44cac913017ba4
SirCrowley <shiky0@live.fr>
2016-02-26 09:55:13 +0100

    appel eleve / professeur  methode findAll*

--- controleurs/app.php
+++ controleurs/app.php
@@ -33,28 +33,29 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     ),
 ));
 
-$app['security.role_hierarchy'] = array(
-    'ROLE_ADMIN'    => array('ROLE_PROF', 'ROLE_ELEVE', 'ROLE_USER'),
-    'ROLE_PROF'    => array('ROLE_ELEVE', 'ROLE_USER'),
-    'ROLE_ELEVE'    => array('ROLE_USER')
-);
+//$app['security.role_hierarchy'] = array(
+//    'ROLE_ADMIN'    => array('ROLE_PROF', 'ROLE_ELEVE', 'ROLE_USER'),
+//    'ROLE_PROF'    => array('ROLE_ELEVE', 'ROLE_USER'),
+//    'ROLE_ELEVE'    => array('ROLE_USER')
+//);
 
 // Definition des rôles utilisateurs
-//'security.access_rules' => array(
-//    array('^/acceuil', 'ROLE_USER'),
-//    array('^/layout', 'ROLE_USER'),
-//    array('^/activite/', 'ROLE_ELEVE'),
-//    array('^/activite/new', 'ROLE_ELEVE'),
-//    array('^/competence/', 'ROLE_ELEVE'),
-//    array('^/competence/new', 'ROLE_ELEVE'),
-//    array('^/competence/.*$', 'ROLE_PROF'),
-//    array('^/activite/.*$', 'ROLE_PROF'),
-//    array('^/eleves/.*$', 'ROLE_PROF'),
-//    array('^/promotion/.*$', 'ROLE_PROF'),
-//    array('^/professeurs/.*$', 'ROLE_ADMIN'),
-//    array('^/', 'ROLE_USER'),
-//    array('^/login', ''), //Cette url est accessible en mode non connecté
-//);
+'security.access_rules' => array(
+    array('^/login', ''), //Cette url est accessible en mode non connecté
+    array('^/acceuil', 'ROLE_USER'),
+    array('^/layout', 'ROLE_USER'),
+    array('^/activite/', 'ROLE_ELEVE'),
+    array('^/activite/new', 'ROLE_ELEVE'),
+    array('^/competence/', 'ROLE_ELEVE'),
+    array('^/competence/new', 'ROLE_ELEVE'),
+    array('^/competence/.*$', 'ROLE_PROF'),
+    array('^/activite/.*$', 'ROLE_PROF'),
+    array('^/eleves/.*$', 'ROLE_PROF'),
+    array('^/promotion/.*$', 'ROLE_PROF'),
+    array('^/professeurs/.*$', 'ROLE_ADMIN'),
+    array('^/', 'ROLE_USER'),
+    array('^/login', ''), //Cette url est accessible en mode non connecté
+);
 
 // Register services.
 
