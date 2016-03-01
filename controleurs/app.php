9cc2d4c53697daf5c7edf52659838c71739c1b97
SirCrowley <shiky0@live.fr>
2016-03-01 23:58:30 +0100

    ajout utilisateur admin

--- controleurs/app.php
+++ controleurs/app.php
@@ -2,6 +2,8 @@
 
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+//use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+
 
 // Register global error and exception handlers
 ErrorHandler::register();
@@ -31,6 +33,8 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
             }),
         ),
     ),
+    'security.encoder.digest' => $app->share(function ($app) {
+        return new MySQLPasswordEncoder(false);
 ));
 
 $app['security.role_hierarchy'] = array(
@@ -72,3 +76,6 @@ $app['dao.promotion'] = $app->share(function ($app) {
 $app['dao.utilisateur'] = $app->share(function ($app) {
     return new SIOC\DAO\UtilisateurDAO($app['db']);
 });
+
+
+// creer encrypt du mdp et salt
