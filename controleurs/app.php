da641a6a735284cda147c1a447ea26bfaac6e7bd
SirCrowley <shiky0@live.fr>
2016-03-02 15:14:39 +0100

    fik

--- controleurs/app.php
+++ controleurs/app.php
@@ -29,7 +29,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
             'pattern' => '^.*$',
             'anonymous' => false, // A modifier
             'logout' => array('logout_path' => '/logout'),
-            'form' => array('login_path' => 'login', 'check_path' => 'login_check'), //Bug double redirection
+            'form' => array('login_path' => 'login', 'check_path' => 'login_check'), //Bug double redirection // mlol
             'users' => array(
                 'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
             ),
