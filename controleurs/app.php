61dc33b3719bc3c9b6232be7987781f3cea3d791
SirCrowley <shiky0@live.fr>
2016-03-02 15:12:35 +0100

    anon false

--- controleurs/app.php
+++ controleurs/app.php
@@ -25,14 +25,9 @@ $app->register(new Silex\Provider\SessionServiceProvider());
  */
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
-//        'login' => array(
-//            'pattern' => '^/login$',
-//            'anonymous' => true,
-//            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
-//        ),
         'secured' => array(
             'pattern' => '^.*$',
-            'anonymous' => true, // A modifier
+            'anonymous' => false, // A modifier
             'logout' => array('logout_path' => '/logout'),
             'form' => array('login_path' => '/login', 'check_path' => 'login_check'), //Bug double redirection
             'users' => array(
