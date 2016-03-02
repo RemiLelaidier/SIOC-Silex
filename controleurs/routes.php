d850353f1677c41c918536833464688c61782a13
SirCrowley <shiky0@live.fr>
2016-03-02 13:14:17 +0100

    passage parametre var_dump 'login_check'

--- controleurs/routes.php
+++ controleurs/routes.php
@@ -49,7 +49,7 @@ $app->get('/login', function(Request $request) use ($app) {
  */
 $app->get('/test', function() use ($app){
     //echo $app['security.encoder.digest']->encodePassword('0000', '0000');
-    var_dump($app['routes']->all());
+    var_dump($app['login_check']->all());
     die();
 
 });
