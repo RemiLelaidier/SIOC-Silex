0555e2b18927ec79027c588294bbb01134df79d5
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-03-02 11:53:37 +0100

    Fix

--- modeles/DAO/UtilisateurDAO.php
+++ modeles/DAO/UtilisateurDAO.php
@@ -142,7 +142,7 @@ class UtilisateurDAO extends DAO implements UserProviderInterface
      */
     public function loadUserByUsername($username)
     {
-        $sql = "SELECT * FROM Utilisateurs WHERE uti_username=?";
+        $sql = "SELECT * FROM Utilisateur WHERE uti_username=?";
         $row = $this->getDb()->fetchAssoc($sql, array($username));
 
         if ($row)
