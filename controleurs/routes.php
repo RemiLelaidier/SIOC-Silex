2bad3c0e80b0f08baecfdb0ad56ed6c94654a33a
SirCrowley <shiky0@live.fr>
2016-03-04 09:27:27 +0100

    remise à neuf

--- controleurs/routes.php
+++ controleurs/routes.php
@@ -27,11 +27,11 @@ $app->get('/', function () use($app) {
 /**
  * Route export PDF
  */
-$app->get('/exportpdf', function() use ($app) {
-    $app['pdf.generator']->findAll();
-    $competences = $app['dao.competence']->findAll();
-    return $app['twig']->render('exportpdf.html.twig', array('competences' => $competences));
-})->bind('exportpdf');
+//$app->get('/exportpdf', function() use ($app) {
+//    $app['pdf.generator']->findAll();
+//    $competences = $app['dao.competence']->findAll();
+//    return $app['twig']->render('exportpdf.html.twig', array('competences' => $competences));
+//})->bind('exportpdf');
 
 
 //$app->get('/competence', function () use ($app) {
