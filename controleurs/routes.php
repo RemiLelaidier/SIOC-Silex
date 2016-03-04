ecda2a4cf2604b5645d06cd5b2aa63129dfcba0a
SirCrowley <shiky0@live.fr>
2016-03-04 09:21:22 +0100

    fix bug pdf export

--- controleurs/routes.php
+++ controleurs/routes.php
@@ -28,6 +28,7 @@ $app->get('/', function () use($app) {
  * Route export PDF
  */
 $app->get('/exportpdf', function() use ($app) {
+
     $app['pdf.generator']->findAll();
     $competences = $app['dao.competence']->findAll();
     return $app['twig']->render('exportpdf.html.twig', array('competences' => $competences));
