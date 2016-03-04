b1fa86064ab28c01a8202eb4b2ae3609c873a222
SirCrowley <shiky0@live.fr>
2016-03-04 09:23:56 +0100

    remise à neuf

--- controleurs/app.php
+++ controleurs/app.php
@@ -16,8 +16,8 @@ ExceptionHandler::register();
 /**
  * Register service providers.
  */
-$app->register(new Silex\PdfServiceProvider());
-$app->register(new Silex\PdfServiceProviderInterface());
+//$app->register(new Silex\PdfServiceProvider());
+//$app->register(new Silex\PdfServiceProviderInterface());
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../vues',
--- controleurs/routes.php
+++ controleurs/routes.php
@@ -5,7 +5,7 @@ use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 //use Silex\PdfServiceProvider;
 
 /**
- * Route page d'acceuil
+ * Route page d'acceuil //
  * TOCHECK
  */
 $app->get('/', function () use($app) {
@@ -28,7 +28,6 @@ $app->get('/', function () use($app) {
  * Route export PDF
  */
 $app->get('/exportpdf', function() use ($app) {
-
     $app['pdf.generator']->findAll();
     $competences = $app['dao.competence']->findAll();
     return $app['twig']->render('exportpdf.html.twig', array('competences' => $competences));
