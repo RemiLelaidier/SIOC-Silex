85c827267c052233f3fc7be086eadacc3d3b6992
SirCrowley <shiky0@live.fr>
2016-03-04 09:50:39 +0100

    test fix pdf

--- controleurs/app.php
+++ controleurs/app.php
@@ -18,7 +18,7 @@ ExceptionHandler::register();
  * Register service providers.
  */
 
-$app->register(new Erivello\Silex\PdfServiceProvider());
+$app->register(new \Silex\PdfServiceProvider());
 //$app->register(new Silex\PdfServiceProviderInterface());
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
