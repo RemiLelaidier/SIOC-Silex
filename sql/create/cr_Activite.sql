ade261b169d6bad0dcb0cadb862d6415ca9052ee
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-03-09 14:48:34 +0100

    Fix

--- sql/create/cr_Activite.sql
+++ sql/create/cr_Activite.sql
@@ -6,7 +6,7 @@
 CREATE TABLE Activite
 (
     act_id                  INTEGER         auto_increment,
-    act_debut               DATE,
+    act_debut               INTEGER,
     act_duree               INTEGER,
     act_libelle             VARCHAR(35),
     act_description         VARCHAR(350),
