2feb3d2994c3adc158f8232ec423d6a63fac17be
SirCrowley <shiky0@live.fr>
2016-03-09 10:19:25 +0100

    fix

--- controleurs/routes.php
+++ controleurs/routes.php
@@ -206,7 +206,7 @@ $app->post('/utilisateur', function (Request $request) use ($app) {
     $utilisateur->setNom($request->request->get('nom'));
     $utilisateur->setPrenom($request->request->get('prenom'));
     $utilisateur->setMail($request->request->get('email'));
-    $utilisateur->setSalt($salt);
+    $utilisateur->setSalt('0000');
     $encoder = $app['security.encoder.digest'];
     $utilisateur->setPassword($encoder->encodePassword($request->request->get('password'),$utilisateur->getSalt()));
 //    $utilisateur->setRole($request->request->get('statut'));
