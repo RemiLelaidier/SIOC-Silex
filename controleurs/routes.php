38cb00defaf95107fb5947f502ee399a9cbe986a
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-03-14 15:24:14 +0100

    Affichage dynamique Acceuil prof-admin

--- controleurs/routes.php
+++ controleurs/routes.php
@@ -19,16 +19,24 @@ $app->get('/', function () use($app) {
         $nbComp = $app['dao.competence']->findNbByEleve($id);
         $promotion = $app['dao.promotion']->findByEleve($id);
 
-    return $app['twig']->render('acceuil.html.twig', array(
-        'professeurs' => $professeurs,
-        'activites' => $activites,
-        'competences' => $competences,
-        'nbComp' => $nbComp,
-        'promotion' => $promotion
+        return $app['twig']->render('acceuil.html.twig', array(
+            'professeurs' => $professeurs,
+            'activites' => $activites,
+            'competences' => $competences,
+            'nbComp' => $nbComp,
+            'promotion' => $promotion
         ));
     }
- else {
-        return $app['twig']->render('acceuil_admin_prof.html.twig');
+    else {
+        $professeurs = $app['dao.utilisateur']->findAllProfesseur();
+        $eleves = $app['dao.utilisateur']->findAllEleve();
+        $promotions = $app['dao.promotion']->findAll();
+        
+        return $app['twig']->render('acceuil_admin_prof.html.twig', array(
+            'eleves' => $eleves,
+            'professeurs' => $professeurs,
+            'promotions' => $promotions
+        ));
     }
 });
 
