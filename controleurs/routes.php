ff617b08cd03453d984944379e0637720bc200de
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-03-15 19:07:03 +0100

    Fix

--- controleurs/routes.php
+++ controleurs/routes.php
@@ -1,52 +1,52 @@
 <?php
 
 // Route page d'acceuil
-$app->get('/', 'SIOC\Controller\HomeController::homeAction');
+$app->get('/', "SIOC\Controller\HomeController::homeAction");
 
 // Route page de connexion
-$app->get('/login', 'SIOC\Controller\HomeController::loginAction')->bind('login');
+$app->get('/login', "SIOC\Controller\HomeController::loginAction")->bind('login');
 
 // Route page activite
-$app->get('/activite', 'SIOC\Controller\ActiviteController::activiteAction')->bind('activite');
+$app->get('/activite', "SIOC\Controller\ActiviteController::activiteAction")->bind('activite');
 
 // Route Ajout Activite
-$app->get('/activite/new', 'SIOC\Controller\ActiviteController::activiteAjoutAction')->bind('ajout_activite');
+$app->get('/activite/new', "SIOC\Controller\ActiviteController::activiteAjoutAction")->bind('ajout_activite');
 
 // Route Activite{id}
-$app->get('/activite/{id}', 'SIOC\Controller\ActiviteController::activiteIdAction')->bind('activite_');
+$app->get('/activite/{id}', "SIOC\Controller\ActiviteController::activiteIdAction")->bind('activite_');
 
 // Route insertion Activite
-$app->post('/activite', 'SIOC\Controller\ActiviteController::activiteInsertAction');
+$app->post('/activite', "SIOC\Controller\ActiviteController::activiteInsertAction");
 
 // Route Utilisateur
-$app->get('/utilisateur', 'SIOC\Controller\UtilisateurController::utilisateurAction')->bind('utilisateur');
+$app->get('/utilisateur', "SIOC\Controller\UtilisateurController::utilisateurAction")->bind('utilisateur');
 
 // Route Professeur
-$app->get('/professeur', 'SIOC\Controller\UtilisateurController::professeurAction')->bind('professeur');
+$app->get('/professeur', "SIOC\Controller\UtilisateurController::professeurAction")->bind('professeur');
 
 // Route Ajout Utilisateur
-$app->get('/utilisateur/new', 'SIOC\Controller\UtilisateurController::utilisateurAjoutAction')->bind('ajout_utilisateur');
+$app->get('/utilisateur/new', "SIOC\Controller\UtilisateurController::utilisateurAjoutAction")->bind('ajout_utilisateur');
 
 // Route Insertion Utilisateur
-$app->post('/utilisateur', 'SIOC\Controller\UtilisateurController::utilisateurInserAction');
+$app->post('/utilisateur', "SIOC\Controller\UtilisateurController::utilisateurInserAction");
 
 //Route Competence
-$app->get('/competence', 'SIOC\Controller\CompetenceController::competenceAction')->bind('competence');
+$app->get('/competence', "SIOC\Controller\CompetenceController::competenceAction")->bind('competence');
 
 // Route Ajout Competence
-$app->get('/competence/new', 'SIOC\Controller\CompetenceController::competenceAjoutAction')->bind('ajout_competence');
+$app->get('/competence/new', "SIOC\Controller\CompetenceController::competenceAjoutAction")->bind('ajout_competence');
 
 // Route Insert Competence
-$app->post('/competence', 'SIOC\Controller\CompetenceController::competenceInsertAction');
+$app->post('/competence', "SIOC\Controller\CompetenceController::competenceInsertAction");
 
 // Route Promotion
-$app->get('/promotion', 'SIOC\Controller\PromotionController::promotionAction')->bind('promotion');
+$app->get('/promotion', "SIOC\Controller\PromotionController::promotionAction")->bind('promotion');
 
 // Route Ajout Promotion
-$app->get('/promotion/new', 'SIOC\Controller\PromotionController::promotionAjoutAction')->bind('ajout_promotion');
+$app->get('/promotion/new', "SIOC\Controller\PromotionController::promotionAjoutAction")->bind('ajout_promotion');
 
 // Route Insert Promotion
-$app->post('/promotion', 'SIOC\Controller\PromotionController::promotionInsertAction');
+$app->post('/promotion', "SIOC\Controller\PromotionController::promotionInsertAction");
 
 // Route Promotion{id}
-$app->get('/promotion/{id}', 'SIOC\Controller\PromotionController::promotionIdAction')->bind('promotion/{id}');
+$app->get('/promotion/{id}', "SIOC\Controller\PromotionController::promotionIdAction")->bind('promotion/{id}');
