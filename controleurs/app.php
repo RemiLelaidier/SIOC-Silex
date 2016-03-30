dd241c0efd52070d19006fa96f7f29a92c6cfd00
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-03-30 09:53:26 +0200

    Fix

--- controleurs/app.php
+++ controleurs/app.php
@@ -52,7 +52,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
         ),
         'secured' => array(
             'pattern' => '^.*$',
-            'anonymous' => false,
+            'anonymous' => true,
             'logout' => true,
             'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
             'users' =>$app->share(function () use ($app) {
