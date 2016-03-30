e16da4b9588cb95a46cd68cccc9c417843e90b67
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-03-30 10:04:14 +0200

    Test

--- controleurs/app.php
+++ controleurs/app.php
@@ -52,7 +52,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
         ),
         'secured' => array(
             'pattern' => '^.*$',
-            'anonymous' => true,
+            'anonymous' => false,
             'logout' => true,
             'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
             'users' =>$app->share(function () use ($app) {
