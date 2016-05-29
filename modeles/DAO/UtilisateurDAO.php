bbe7e1126c1a1a29fc8893f5605ecb1bad98b6f0
RemiLelaidier <r.lelaidier@hotmail.fr>
2016-05-29 15:03:09 +0200

    Undo Test

--- modeles/DAO/UtilisateurDAO.php
+++ modeles/DAO/UtilisateurDAO.php
@@ -122,8 +122,8 @@ class UtilisateurDAO extends DAO implements UserProviderInterface
             $eleveId = $row['uti_id'];
             $promotion = new PromotionDAO($this->getDb());
             $cursus = new CursusDAO($this->getDb());
-            //$row['uti_promotion'] = $promotion->findByEleve($eleveId);
-            //$row['uti_cursus'] = $cursus->findByEleve($eleveId);
+            $row['uti_promotion'] = $promotion->findByEleve($eleveId);
+            $row['uti_cursus'] = $cursus->findByEleve($eleveId);
             $eleves[$eleveId] = $this->buildDomainObject($row);
         }
         return $eleves;
